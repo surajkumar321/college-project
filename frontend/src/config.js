47bb76c0541b2a62c93b8634cea218e878be33f0
delta student <kumardharmraj703@gmail.com>
2025-09-15 15:40:49 +0530

    Set API to render backend URL

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 // frontend/src/config.js
-export const API = "http://localhost:5000/api";
+export const API = "https://college-project-y397.onrender.com/api";
 export const getUserId = () => localStorage.getItem("userId");
 export const getToken = () => localStorage.getItem("token");
 export const setAuth = ({ token, userId }) => { localStorage.setItem("token", token); localStorage.setItem("userId", userId); }
